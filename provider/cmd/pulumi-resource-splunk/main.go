f9e789129db902c6fa0dda1cb1a60ba5ad1885c0
Ian Wahbe <ian@wahbe.com>
2024-01-13 18:56:19 -0800

    lint_provider.fix

--- provider/cmd/pulumi-resource-splunk/main.go
+++ provider/cmd/pulumi-resource-splunk/main.go
@@ -19,9 +19,10 @@ package main
 import (
 	_ "embed"
 
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+
 	splunk "github.com/pulumi/pulumi-splunk/provider"
 	"github.com/pulumi/pulumi-splunk/provider/pkg/version"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
 
 //go:embed schema-embed.json
